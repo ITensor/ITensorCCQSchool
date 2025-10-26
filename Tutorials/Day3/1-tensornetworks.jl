55c7b2b6c1a61a8f8aa41f42488a0e2a74f7dad2
Matt Fishman <mtfishman@users.noreply.github.com>
2025-10-25 21:20:53 -0400

    Import Graphs.jl functions from Graphs.jl (#13)

--- Tutorials/Day3/1-tensornetworks.jl
+++ Tutorials/Day3/1-tensornetworks.jl
@@ -1,4 +1,5 @@
-using NamedGraphs: NamedEdge, NamedGraph, add_edges
+using Graphs: add_edge!
+using NamedGraphs: NamedGraph, NamedEdge
 using NamedGraphs.NamedGraphGenerators: named_path_graph
 
 include("ising_tensornetwork.jl")
@@ -19,12 +20,15 @@ A named tuple containing:
 - `tn::Dict{Any, ITensor}`: The tensor network representation of the Ising model on the graph.
 - `g::NamedGraph`: The created graph.
 - `z::Number`: The partition function Z computed by contracting the tensor network.
+- `beta::Number`: Same as above.
 """
 function main(; beta::Number = 0.2, outputlevel::Int = 1)
     # Create a simple graph
     g = NamedGraph([1, 2, 3])
-    edges = [1 => 2, 2 => 3]
-    g = add_edges(g, edges)
+    es = [1 => 2, 2 => 3]
+    for e in es
+        add_edge!(g, e)
+    end
 
     # Construct the tensor network of the classical Ising partition function on the graph
     tn = ising_tensornetwork(g, beta)
@@ -32,5 +36,5 @@ function main(; beta::Number = 0.2, outputlevel::Int = 1)
     # Contract the tensor network to compute the partition function Z
     z = contract_network(tn, g)[]
 
-    return (; tn, g, z)
+    return (; tn, g, z, beta)
 end
